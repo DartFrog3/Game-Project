e7eeb707c1accec67eeb12e2cf5198b8ec5d22dc
DartFrog3 <colerobins03@gmail.com>
2020-09-16 12:50:42 -0700

    Pre-Made Drawing
    I am not totally sure why, maybe the master was changed?
    
    Altered and switched around colors.

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -4,16 +4,16 @@ function setup() {
 
 function draw(){
 	background(220,100,3);
-  fill("black")
-  ellipse(100,100,40,50);
   fill("blue")
+  ellipse(100,100,40,50);
+  fill("black")
   rect(200,200,100,150)
   rect(400,300,100,100);
-  fill("green");
+  fill("yellow");
   rect(375,400,150,150);
   rect(325,410,50,100);
   rect(500,410,50,100);
-  fill("yellow");
+  fill("green");
   ellipse(400,340,15,15);
   ellipse(450,340,15,15);
 }
