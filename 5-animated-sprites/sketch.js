346731c2b80a83e194eddbf56a646c9d0b38d565
DartFrog3 <colerobins03@gmail.com>
2020-09-24 08:34:35 -0700

    Animation and Sprites

--- 5-animated-sprites/sketch.js
+++ 5-animated-sprites/sketch.js
@@ -2,7 +2,7 @@
 //
 // animations like p5 images should be stored in variables
 // in order to be displayed during the draw cycle
-var ghost;
+var cow;
 //
 // //it's advisable (but not necessary) to load the images in the preload function
 // //of your sketch otherwise they may appear with a little delay
@@ -10,7 +10,7 @@ function preload() {
 //
 //   //create an animation from a sequence of numbered images
 //   //pass the first and the last file name and it will try to find the ones in between
-  ghost = loadAnimation('assets/000.png','assets/002.png');
+  cow = loadAnimation('assets/cow001.png','assets/cow002.png', 'assets/cow003.png','assets/cow004.png', 'assets/cow005.png');
 
 }
 
@@ -23,6 +23,6 @@ function draw() {
 
   //specify the animation instance and its x,y position
   //animation() will update the animation frame as well
-  animation(ghost, 300, 150);
+  animation(cow, 300, 150);
 
 }
